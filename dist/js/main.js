730c810d29bd39fe719269fa6355cccda645bb19
Alex Regan <alex.regan@artisancolour.com>
2017-05-15 06:05:20 -0700

    countdown advances +1 after finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,119 +5,143 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 (function () {
-	'use strict';
+    'use strict';
 
-	var Countdown = function () {
-		_createClass(Countdown, null, [{
-			key: 'leftPadZero',
-			value: function leftPadZero(t) {
-				var digits = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
+    var Countdown = function () {
+        _createClass(Countdown, null, [{
+            key: 'leftPadZero',
+            value: function leftPadZero(t) {
+                var digits = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
 
-				var pad = '';
-				for (var i = digits - 1; i > 0; i--) {
-					pad += '0';
-				}return ('' + pad + t).slice(-digits);
-			}
-		}]);
+                var pad = '';
+                for (var i = digits - 1; i > 0; i--) {
+                    pad += '0';
+                }return ('' + pad + t).slice(-digits);
+            }
+        }]);
 
-		function Countdown(endDate) {
-			var elSelector = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : Countdown.DEFAULTS.selector;
+        function Countdown(endDate) {
+            var elSelector = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : Countdown.DEFAULTS.selector;
 
-			_classCallCheck(this, Countdown);
+            _classCallCheck(this, Countdown);
 
-			this.countdownDate = endDate instanceof Date ? endDate.getTime() : new Date(endDate).getTime();
+            this.countdownDate = endDate instanceof Date ? endDate.getTime() : new Date(endDate).getTime();
 
-			this.element = document.querySelector(elSelector);
-			this.intervalId = null;
-			this.countdown = this.countdown.bind(this);
-		}
+            this.element = document.querySelector(elSelector);
+            this.intervalId = null;
+            this.countdown = this.countdown.bind(this);
+        }
 
-		_createClass(Countdown, [{
-			key: 'start',
-			value: function start() {
-				var interval = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : Countdown.SECOND;
-				var runAtStart = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+        _createClass(Countdown, [{
+            key: 'start',
+            value: function start() {
+                var interval = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : Countdown.SECOND;
+                var runAtStart = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
 
-				if (runAtStart) this.countdown();
-				this.intervalId = window.setInterval(this.countdown, interval);
+                if (runAtStart) this.countdown();
+                this.intervalId = window.setInterval(this.countdown, interval);
 
-				return this;
-			}
-		}, {
-			key: 'stop',
-			value: function stop() {
-				if (this.intervalId !== null) {
-					window.clearInterval(this.intervalId);
-				}
+                return this;
+            }
+        }, {
+            key: 'stop',
+            value: function stop() {
+                if (this.intervalId !== null) {
+                    window.clearInterval(this.intervalId);
+                }
 
-				return this;
-			}
-		}, {
-			key: 'countdown',
-			value: function countdown() {
-				var t = this.getTime();
+                return this;
+            }
+        }, {
+            key: 'countdown',
+            value: function countdown() {
+                var t = this.getTime();
 
-				if (t.total > 0) {
-					this.render(t);
-				} else {
-					this.stop().renderComplete();
-				}
+                if (t.total > 0) {
+                    this.render(t);
+                } else {
+                    this.stop().renderComplete();
+                }
 
-				return this;
-			}
-		}, {
-			key: 'getTime',
-			value: function getTime() {
-				// Get today's date and time
-				var now = new Date().getTime();
+                return this;
+            }
+        }, {
+            key: 'getTime',
+            value: function getTime() {
+                // Get today's date and time
+                var now = new Date().getTime();
 
-				// Find the distance between now an the count down date in ms
-				var total = this.countdownDate - now;
+                // Find the distance between now an the count down date in ms
+                var total = this.countdownDate - now;
 
-				var d = Math.floor(total / Countdown.DAY);
-				var h = Math.floor(total % Countdown.DAY / Countdown.HOUR);
-				var m = Math.floor(total % Countdown.HOUR / Countdown.MINUTE);
-				var s = Math.floor(total % Countdown.MINUTE / Countdown.SECOND);
-				var ms = Math.floor(total % Countdown.SECOND);
+                var d = Math.floor(total / Countdown.DAY);
+                var h = Math.floor(total % Countdown.DAY / Countdown.HOUR);
+                var m = Math.floor(total % Countdown.HOUR / Countdown.MINUTE);
+                var s = Math.floor(total % Countdown.MINUTE / Countdown.SECOND);
+                var ms = Math.floor(total % Countdown.SECOND);
 
-				return { d: d, h: h, m: m, s: s, ms: ms, total: total };
-			}
-		}, {
-			key: 'render',
-			value: function render(t) {
-				// format fn
-				var f = Countdown.leftPadZero;
-				var times = [t.d + 'd', f(t.h) + 'h', f(t.m) + 'm', f(t.s) + 's'];
+                return { d: d, h: h, m: m, s: s, ms: ms, total: total };
+            }
+        }, {
+            key: 'render',
+            value: function render(t) {
+                // format fn
+                var f = Countdown.leftPadZero;
+                var times = [t.d + 'd', f(t.h) + 'h', f(t.m) + 'm', f(t.s) + 's'];
 
-				this.element.innerHTML = times.join('&nbsp;&nbsp;');
+                this.element.innerHTML = times.join('&nbsp;&nbsp;');
 
-				return this;
-			}
-		}, {
-			key: 'renderComplete',
-			value: function renderComplete() {
-				this.element.innerHTML = Countdown.DEFAULTS.doneText;
+                return this;
+            }
+        }, {
+            key: 'renderComplete',
+            value: function renderComplete() {
+                this.element.innerHTML = Countdown.DEFAULTS.doneText;
 
-				return this;
-			}
-		}]);
+                return this;
+            }
+        }], [{
+            key: 'SECOND',
+            get: function get() {
+                return 1000;
+            }
+        }, {
+            key: 'MINUTE',
+            get: function get() {
+                return 60 * Countdown.SECOND;
+            }
+        }, {
+            key: 'HOUR',
+            get: function get() {
+                return 60 * Countdown.MINUTE;
+            }
+        }, {
+            key: 'DAY',
+            get: function get() {
+                return 24 * Countdown.HOUR;
+            }
+        }, {
+            key: 'DEFAULTS',
+            get: function get() {
+                return {
+                    selector: '#countdown-display',
+                    doneText: "EXPIRED"
+                };
+            }
+        }]);
 
-		return Countdown;
-	}();
+        return Countdown;
+    }();
 
-	Countdown.SECOND = 1000;
-	Countdown.MINUTE = 60 * Countdown.SECOND;
-	Countdown.HOUR = 60 * Countdown.MINUTE;
-	Countdown.DAY = 24 * Countdown.HOUR;
-	Countdown.DEFAULTS = {
-		selector: '#countdown-display',
-		doneText: "EXPIRED"
-	};
+    var countdownDate = "May 16, 2017 12:00:00 GMT-0700";
+    var datetime = new Date(countdownDate);
 
-	var countdownDate = "May 16, 2017 12:00:00 GMT-0700";
+    if (Date.now() > datetime.getTime()) {
+        datetime.setDate(datetime.getDate() + 1);
+    }
 
-	window.countdown = new Countdown(countdownDate);
+    window.countdown = new Countdown(datetime);
 
-	countdown.start();
+    countdown.start();
 })();
 //# sourceMappingURL=main.js.map
--- js/main.js
+++ js/main.js
@@ -1,96 +1,115 @@
-(function () {
-	'use strict';
-	
-	class Countdown {
-		static leftPadZero( t, digits = 2 ) {
-			let pad = '';
-			for ( let i = digits - 1; i > 0; i-- ) pad += '0';
-			
-			return `${pad}${t}`.slice( -digits );
-		}
-		
-		constructor( endDate, elSelector = Countdown.DEFAULTS.selector ) {
-			this.countdownDate = endDate instanceof Date
-				? endDate.getTime()
-				: new Date( endDate ).getTime();
-			
-			this.element    = document.querySelector( elSelector );
-			this.intervalId = null;
-			this.countdown  = this.countdown.bind( this );
-		}
-		
-		start( interval = Countdown.SECOND, runAtStart = true ) {
-			if ( runAtStart ) this.countdown();
-			this.intervalId = window.setInterval( this.countdown, interval );
-			
-			return this;
-		}
-		
-		stop() {
-			if ( this.intervalId !== null ) {
-				window.clearInterval( this.intervalId );
-			}
-			
-			return this;
-		}
-		
-		countdown() {
-			const t = this.getTime();
-			
-			if ( t.total > 0 ) {
-				this.render( t )
-			} else {
-				this.stop().renderComplete();
-			}
-			
-			return this;
-		}
-		
-		getTime() {
-			// Get today's date and time
-			const now = new Date().getTime();
-			
-			// Find the distance between now an the count down date in ms
-			const total = this.countdownDate - now;
-			
-			const d  = Math.floor( total / Countdown.DAY );
-			const h  = Math.floor( (total % Countdown.DAY) / Countdown.HOUR );
-			const m  = Math.floor( (total % Countdown.HOUR) / Countdown.MINUTE );
-			const s  = Math.floor( (total % Countdown.MINUTE) / Countdown.SECOND );
-			const ms = Math.floor( total % Countdown.SECOND );
-			
-			return { d, h, m, s, ms, total };
-		}
-		
-		render( t ) {
-			// format fn
-			const f     = Countdown.leftPadZero;
-			const times = [ `${t.d}d`, `${f( t.h )}h`, `${f( t.m )}m`, `${f( t.s )}s` ];
-			
-			this.element.innerHTML = times.join( `&nbsp;&nbsp;` );
-			
-			return this;
-		}
-		
-		renderComplete() {
-			this.element.innerHTML = Countdown.DEFAULTS.doneText;
-			
-			return this;
-		}
-	}
-	
-	Countdown.SECOND   = 1000;
-	Countdown.MINUTE   = 60 * Countdown.SECOND;
-	Countdown.HOUR     = 60 * Countdown.MINUTE;
-	Countdown.DAY      = 24 * Countdown.HOUR;
-	Countdown.DEFAULTS = {
-		selector: '#countdown-display',
-		doneText: "EXPIRED",
-	};
-	
-	const countdownDate = "May 16, 2017 12:00:00 GMT-0700";
-	
-	window.countdown = new Countdown( countdownDate );
-	
-	countdown.start();
+(function() {
+    'use strict';
+
+    class Countdown {
+        static leftPadZero(t, digits = 2) {
+            let pad = ''
+            for (let i = digits - 1; i > 0; i--) pad += '0'
+
+            return `${pad}${t}`.slice(-digits)
+        }
+
+        constructor(endDate, elSelector = Countdown.DEFAULTS.selector) {
+            this.countdownDate = endDate instanceof Date
+                ? endDate.getTime()
+                : new Date(endDate).getTime()
+
+            this.element = document.querySelector(elSelector)
+            this.intervalId = null
+            this.countdown = this.countdown.bind(this)
+        }
+
+        start(interval = Countdown.SECOND, runAtStart = true) {
+            if (runAtStart) this.countdown()
+            this.intervalId = window.setInterval(this.countdown, interval)
+
+            return this
+        }
+
+        stop() {
+            if (this.intervalId !== null) {
+                window.clearInterval(this.intervalId)
+            }
+
+            return this;
+        }
+
+        countdown() {
+            const t = this.getTime()
+
+            if (t.total > 0) {
+                this.render(t)
+            } else {
+                this.stop().renderComplete()
+            }
+
+            return this
+        }
+
+        getTime() {
+            // Get today's date and time
+            const now = new Date().getTime()
+
+            // Find the distance between now an the count down date in ms
+            const total = this.countdownDate - now
+
+            const d = Math.floor(total / Countdown.DAY)
+            const h = Math.floor((total % Countdown.DAY) / Countdown.HOUR)
+            const m = Math.floor((total % Countdown.HOUR) / Countdown.MINUTE)
+            const s = Math.floor((total % Countdown.MINUTE) / Countdown.SECOND)
+            const ms = Math.floor(total % Countdown.SECOND)
+
+            return { d, h, m, s, ms, total }
+        }
+
+        render(t) {
+            // format fn
+            const f = Countdown.leftPadZero;
+            const times = [`${t.d}d`, `${f( t.h )}h`, `${f( t.m )}m`, `${f( t.s )}s`]
+
+            this.element.innerHTML = times.join(`&nbsp;&nbsp;`)
+
+            return this
+        }
+
+        renderComplete() {
+            this.element.innerHTML = Countdown.DEFAULTS.doneText
+
+            return this
+        }
+
+        static get SECOND() {
+            return 1000
+        }
+
+        static get MINUTE() {
+            return 60 * Countdown.SECOND
+        }
+
+        static get HOUR() {
+            return 60 * Countdown.MINUTE
+        }
+
+        static get DAY() {
+            return 24 * Countdown.HOUR
+        }
+
+        static get DEFAULTS() {
+            return {
+                selector: '#countdown-display',
+                doneText: "EXPIRED",
+            }
+        }
+    }
+
+    const countdownDate = "May 16, 2017 12:00:00 GMT-0700";
+    let datetime = new Date(countdownDate)
+
+    if (Date.now() > datetime.getTime()) {
+        datetime.setDate(datetime.getDate() + 1)
+    }
+
+    window.countdown = new Countdown(datetime);
+
+    countdown.start();
 }());
